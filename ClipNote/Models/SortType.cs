51c52f0077aad0e2b82f70a212fe04b9e5ea77ad
Akn <sibata486@gmail.com>
2023-11-30 20:42:56 +0900

    add feat / リストをソートする機能を実装

---
+++ ClipNote/Models/SortType.cs
@@ -0,0 +1,10 @@
+namespace ClipNote.Models
+{
+    public enum SortType
+    {
+#pragma warning disable SA1602
+        Type,
+        DateTime,
+#pragma warning restore SA1602
+    }
+}
--- ClipNote/ViewModels/MainWindowViewModel.cs
+++ ClipNote/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using ClipNote.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace ClipNote.ViewModels
@@ -9,8 +12,37 @@ namespace ClipNote.ViewModels
     {
         private string title = "Prism Application";
 
+        private ObservableCollection<Text> texts = new ObservableCollection<Text>();
+
         public string Title { get => title; set => SetProperty(ref title, value); }
 
-        public ObservableCollection<Text> Texts { get; set; } = new ObservableCollection<Text>();
+        public ObservableCollection<Text> Texts
+        {
+            get => texts;
+            private set
+            {
+                if (SetProperty(ref texts, value))
+                {
+                    RaisePropertyChanged(nameof(SortCommand));
+                }
+            }
+        }
+
+        public DelegateCommand<object> SortCommand => new DelegateCommand<object>((param) =>
+        {
+            var sortType = (SortType)param;
+
+            switch (sortType)
+            {
+                case SortType.Type:
+                    Texts = new ObservableCollection<Text>(Texts.OrderBy(t => t.Type));
+                    break;
+                case SortType.DateTime:
+                    Texts = new ObservableCollection<Text>(Texts.OrderBy(t => t.CreatedAt));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        });
     }
 }
