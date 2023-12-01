c0b6a7c4b246bf81de2ca6ac850221bb22734887
Akn <sibata486@gmail.com>
2023-12-01 22:23:35 +0900

    fix feat / テキストがファイル名かの判定を厳しくした
    従来はガバガバだったので修正した。

--- ClipNoteTests/Models/TextTest.cs
+++ ClipNoteTests/Models/TextTest.cs
@@ -18,5 +18,24 @@ namespace ClipNoteTests.Models
             Assert.That(new Text("001").Type, Is.EqualTo(TextType.Number), "整数なので数値として判定される");
             Assert.That(new Text("1.1").Type, Is.EqualTo(TextType.Number), "実数でも数値で判定される");
         }
+
+        [Test]
+        public void FileNameの判定テスト()
+        {
+            // ファイル名として判定されるもの
+            Assert.That(new Text("test.txt").Type, Is.EqualTo(TextType.FileName), "ドットが間に入っているのでファイル名で判定");
+            Assert.That(new Text("test-test_test.txt").Type, Is.EqualTo(TextType.FileName), "ハイフンとアンダーバーはファイル名");
+
+            // ファイル名ではないと判定されるもの
+            Assert.That(new Text("test.").Type, Is.EqualTo(TextType.Text), "最後にだけドットがついてるものはファイル名ではない");
+            Assert.That(new Text("test.a\r").Type, Is.EqualTo(TextType.Text), "改行コードを含むものはファイル名ではない");
+            Assert.That(new Text("test.a\n").Type, Is.EqualTo(TextType.Text), "改行コードを含むものはファイル名ではない");
+            Assert.That(new Text("test.a\r\n").Type, Is.EqualTo(TextType.Text), "改行コードを含むものはファイル名ではない");
+            Assert.That(new Text("test$.a").Type, Is.EqualTo(TextType.Text), "ハイフンとアンダーバー以外の記号を含むとファイル名ではない");
+            Assert.That(new Text("test@.a").Type, Is.EqualTo(TextType.Text), "ハイフンとアンダーバー以外の記号を含むとファイル名ではない");
+            Assert.That(new Text("test#.a").Type, Is.EqualTo(TextType.Text), "ハイフンとアンダーバー以外の記号を含むとファイル名ではない");
+            Assert.That(new Text("test test.a").Type, Is.EqualTo(TextType.Text), "空白を含むものはファイル名ではない");
+            Assert.That(new Text("test\ttest.a").Type, Is.EqualTo(TextType.Text), "空白を含むものはファイル名ではない");
+        }
     }
 }
